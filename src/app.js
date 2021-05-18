6e71a25e3ad79cd9a05b20c94debd44df40a5595
Jandomen <79184064+Jandomen@users.noreply.github.com>
2021-05-18 09:18:03 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path    = require('path');
+const logger  = require('morgan');
+
+const app = express();
+
+
+app.set('port', 5000);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(logger('dev'));
+app.use(express.urlencoded({extended: false}));
+
+
+app.use(require('./routes/index'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use((req, res, next) => {
+  res.status(404).render('404');
+});
+
+module.exports = app;
